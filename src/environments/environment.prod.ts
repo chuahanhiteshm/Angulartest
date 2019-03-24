db252910249b18ad8566637f44ad050aacc0b82b
chauhanhiteshm@gmail.com <chauhanhiteshm@gmail.comm>
2019-03-25 02:02:19 +0530

    fdf

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCrSBGmQDf9YNRwBKhAc8BbCECKIjjeSqs",
+    authDomain: "hiteshshop.firebaseapp.com",
+    databaseURL: "https://hiteshshop.firebaseio.com",
+    projectId: "hiteshshop",
+    storageBucket: "hiteshshop.appspot.com",
+    messagingSenderId: "269151253635"
+  }
+};
